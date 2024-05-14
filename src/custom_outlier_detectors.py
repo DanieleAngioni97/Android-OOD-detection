baef6a3b4282a4e26a0094497d0eb9c421f5a2f5
TrueSalmonKing <hichameddoubi@gmail.com>
2024-05-14 10:14:41 +0200

    Added pipeline using Pytorch OOD adhoc methods

--- src/custom_outlier_detectors.py
+++ src/custom_outlier_detectors.py
@@ -1,5 +1,5 @@
 import sklearn.svm
-
+import torch
 
 class MaxConfidence:
     def __init__(self, clf,
@@ -22,6 +22,9 @@ class MaxConfidence:
         conf = self.predict_proba(X_test).max(axis=1)
         mask = conf < self.threshold
         pred[mask] = -1
+        
+        # print(mask, pred[mask])
+        
         return pred
 
     def set_threshold(self, X):
@@ -30,3 +33,34 @@ class MaxConfidence:
         n_rejected_samples = int(self.rej_perc * X.shape[0])
         self.threshold = maxconf[n_rejected_samples]
 
+
+class OODDetector:
+    def __init__(self, clf, detector,
+                 rej_perc=0.15):
+        self.clf = clf
+        self.detector = detector
+        self.rej_perc = rej_perc
+        self.threshold = 0.1
+
+    def fit(self, *args):
+        pass
+
+    def predict_proba(self, X):
+        if isinstance(self.clf, sklearn.svm.LinearSVC):
+            return self.clf._predict_proba_lr(X)
+        else:
+            return self.clf.predict_proba(X)
+
+    def predict(self, X_test):
+        pred = self.predict_proba(X_test).argmax(axis=1)
+        conf = self.detector.score(torch.from_numpy(self.clf.predict_proba(X_test))).numpy()
+        mask = conf > self.threshold
+        pred[mask] = -1
+        
+        return pred
+
+    def set_threshold(self, X):
+        maxconf = self.detector.score(torch.from_numpy(self.predict_proba(X))).numpy()
+        maxconf[::-1].sort()
+        n_rejected_samples = int(self.rej_perc * X.shape[0])
+        self.threshold = maxconf[n_rejected_samples]
